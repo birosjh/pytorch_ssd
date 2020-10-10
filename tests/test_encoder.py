ccc5c2999ccfc9d78b5e13fa8827cd7ee3a6ec04
jakebiros <jakebiros@gmail.com>
2020-10-11 01:13:42 +0900

    update test to confirm that the number of default boxes is correct

--- tests/test_encoder.py
+++ tests/test_encoder.py
@@ -6,7 +6,7 @@ from data_encoder import DataEncoder
 class TestEncoder(unittest.TestCase):
 
     def setUp(self):
-        config = { 
+        self.config = { 
             "figure_size": 300,
             "feature_map_sizes": [38, 19, 10, 5, 3, 1],
             "steps": [8, 16, 32, 64, 100, 300],
@@ -15,11 +15,17 @@ class TestEncoder(unittest.TestCase):
             "feature_maps": [38, 19, 10, 5, 3, 1]
         }
 
-        self.data_encoder = DataEncoder(config)
+        self.data_encoder = DataEncoder(self.config)
 
     def test_default_box_shape(self):
 
-        print(len(self.data_encoder.default_boxes))
+        total_num_boxes = 0
+        for idx, feature_map in enumerate(self.config['feature_maps']):
+            num_aspect_ratios = 2 + len(self.config['aspect_ratios'][idx]) * 2
+            total_num_boxes += (feature_map * feature_map) * num_aspect_ratios
+
+        self.assertEqual(total_num_boxes, len(self.data_encoder.default_boxes))
+        
 
 if __name__ == '__main__':
     unittest.main()
