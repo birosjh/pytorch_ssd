a37ea9a7808581900951579dadd2fc2485be58be
jakebiros <jakebiros@gmail.com>
2021-01-31 17:30:28 +0900

    Roughly redo dataset to use VOC files

--- image_dataset.py
+++ image_dataset.py
@@ -4,6 +4,7 @@ import json
 import torch
 
 import numpy as np
+import pandas as pd
 import imgaug.augmenters as iaa
 
 from torch.utils.data import Dataset
@@ -13,42 +14,27 @@ from data_encoder import DataEncoder
 
 class ImageDataset(Dataset):
 
-    def __init__(self, data_config, transform=True, train=True):
+    def __init__(self, data_config, transform=True, mode="train"):
 
-        self.train = train
-
+        # Read in necessary configs
+        file_data_path = data_config[mode]
         self.image_directory = data_config["image_directory"]
+        self.annotations_directory = data_config["annotations_directory"]
+
+        self.file_list = self.create_file_list(file_data_path)
 
         self.transform = transform
 
         self.height = data_config["figure_size"]
         self.width = data_config["figure_size"]
 
-        self.data_encoder = DataEncoder(data_config)
-
         self.resize_transformation = iaa.Resize({
             "height": self.height,
             "width": self.width
         })
 
-        # Load Annotations Json
-        with open(data_config["annotations"]) as file:
-            annotations_file = file.read()
+        self.data_encoder = DataEncoder(data_config)
 
-        annotation_json = json.loads(annotations_file)
-
-        # Prepare images with annotations
-        self.image_annotations = self.prep_images_and_annotations(
-            annotation_json
-        )
-
-        # Create list of images to draw from using idx
-        self.images_list = list(self.image_annotations.keys())
-
-        # Create list of categories by ID for reference
-        self.categories_by_id = self.prep_categories(
-            annotation_json["categories"]
-        )
 
     def __len__(self):
         return len(self.images_list)
@@ -56,12 +42,12 @@ class ImageDataset(Dataset):
     def __getitem__(self, idx):
 
         # Load image
-        image_file = self.images_list[idx]
-        path_to_file = os.path.join(self.image_directory, image_file)
+        filename = self.file_list[idx]
+        path_to_file = os.path.join(self.image_directory, filename + ".jpg")
         image = cv2.imread(path_to_file)
 
         # Fetch annotations for that image (they are already in bbs format)
-        labels = self.image_annotations[image_file]
+        labels = load_labels_from_annotation(filename)
 
         # Perform transformations on the data
         if self.transform:
@@ -81,53 +67,32 @@ class ImageDataset(Dataset):
 
         return (image, labels)
 
-    def prep_images_and_annotations(self, annotation_json):
+    def create_file_list(self, file_data_path: str) -> list:
 
-        # Split annotation_json
-        images = annotation_json["images"]
-        annotations = annotation_json["annotations"]
+        df = pd.read_csv(file_data_path, names=["filename"])
 
-        # List images by image ID
-        images_by_ids = {} 
+        df['filename'] = df['filename'].str.replace(r"\s\s\d", "")
 
-        for image in images:
-            images_by_ids[image["id"]] = image["file_name"]
+        file_list = df['filename'].tolist()
 
-        # Create a list of image_annotations
-        image_annotations = {}
+        return file_list
+
+    def load_labels_from_annotation(self, filename: str): -> list:
+
+        labels = []
+
+        annotations = []
 
         for annotation in annotations:
 
-            image_name = images_by_ids[
-                annotation['image_id']
-            ]
-
-            # Create bounding box
-            bounding_box = BoundingBox(
-                x1=annotation['bbox'][0],
-                y1=annotation['bbox'][1],
-                x2=annotation['bbox'][0] + annotation['bbox'][2],
-                y2=annotation['bbox'][1] + annotation['bbox'][3],
-                label=annotation['category_id']
-            )
-
-            if image_name in image_annotations.keys():
-
-                image_annotations[image_name].append(
-                    bounding_box
-                )
-
-            else:
-                image_annotations[image_name] = [bounding_box]
-
-        return image_annotations
+            labels.append(BoundingBox(
+                x1=annotation["x1"],
+                y1=annotation["y1"],
+                x2=annotation["x2"],
+                y2=annotation["y2"],
+                label=x1=annotation["label"]
+            ))
 
 
-    def prep_categories(self, categories_json):
+        return labels
 
-        categories_by_id = {}
-
-        for category in categories_json:
-            categories_by_id[category['id']] = category['name']
-
-        return categories_by_id
