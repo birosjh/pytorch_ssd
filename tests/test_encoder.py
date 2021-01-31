4bd3fa1d81f377183035436d4d4e2ecd46c33626
jakebiros <jakebiros@gmail.com>
2021-01-31 21:55:52 +0900

    Update test to use config file

--- tests/test_encoder.py
+++ tests/test_encoder.py
@@ -1,27 +1,25 @@
+import yaml
 import unittest
 
 from data_encoder import DataEncoder
 
-
 class TestEncoder(unittest.TestCase):
 
     def setUp(self):
-        self.config = { 
-            "figure_size": 300,
-            "feature_map_sizes": [38, 19, 10, 5, 3, 1],
-            "steps": [8, 16, 32, 64, 100, 300],
-            "scales": [21, 45, 99, 153, 207, 261, 315],
-            "aspect_ratios": [(2,), (2, 3), (2, 3), (2, 3), (2,), (2,)],
-            "feature_maps": [38, 19, 10, 5, 3, 1]
-        }
 
-        self.data_encoder = DataEncoder(self.config)
+        with open("tests/test_config.yaml") as f:
+            config = yaml.safe_load(f)
+
+        self.data_config = config["data_configuration"]
+
+        self.data_encoder = DataEncoder(self.data_config)
 
     def test_default_box_shape(self):
 
         total_num_boxes = 0
-        for idx, feature_map in enumerate(self.config['feature_maps']):
-            num_aspect_ratios = 2 + len(self.config['aspect_ratios'][idx]) * 2
+        for idx, feature_map in enumerate(self.data_config['feature_maps']):
+            num_aspect_ratios = 2 + \
+                len(self.data_config['aspect_ratios'][idx]) * 2
             total_num_boxes += (feature_map * feature_map) * num_aspect_ratios
 
         self.assertEqual(total_num_boxes, len(self.data_encoder.default_boxes))
