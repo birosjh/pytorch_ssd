d7e13d52a3a98c887efefc62244daab9c5378a78
jakebiros <jakebiros@gmail.com>
2021-03-05 22:14:27 +0900

    initialize model and dataset and pass to trainer

--- train.py
+++ train.py
@@ -1,5 +1,11 @@
 import argparse
+import torch
 import yaml
+
+from explicit_ssd import SSD
+from trainer import Trainer
+from image_dataset import ImageDataset
+
 
 def load_configurations():
 
@@ -15,9 +21,35 @@ def load_configurations():
 
 def main():
 
+    # Use GPU if available
+    device = 'cuda' if torch.cuda.is_available() else 'cpu'
+    print('Using {} device'.format(device))
+
     config = load_configurations()
 
+    model_config = config["model_configuration"]
+    training_config = config["training_configuration"]
+    data_config = config["data_configuration"]
+
     print(config)
+
+    dataset = ImageDataset(
+        data_config=data_config,
+        transform=True,
+    )
+
+    model = SSD().to(device)
+
+    trainer = Trainer(
+        model, 
+        dataset,
+        loss_function, 
+        training_config
+    )
+
+
+
+
 
 
 if __name__ == "__main__":
