835e11199808f9bae85a9c48ddd84105be4d0e8d
jakebiros <jakebiros@gmail.com>
2021-05-27 22:50:33 +0900

    Add a layers folder for containing common layers

---
+++ models/layers/downsample_block.py
@@ -0,0 +1,24 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class DownsampleBlock(nn.Module):
+
+    def __init__(self, in_channels, out_channels):
+
+        super(DownsampleBlock, self).__init__()
+
+        self.block = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels,
+                      kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(out_channels, out_channels,
+                      kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(2)
+        )
+
+    def forward(self, x):
+
+        return self.block(x)
