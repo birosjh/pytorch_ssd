93ac185d7cdccc876b9fe31c71cd0d139830e026
jakebiros <jakebiros@gmail.com>
2021-05-27 22:49:56 +0900

    Create backbone models

---
+++ models/backbone/simple.py
@@ -0,0 +1,29 @@
+
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+from models.layers.downsample_block import DownsampleBlock
+
+class SimpleBackbone(nn.Module):
+
+    def __init__(self, layers):
+
+        super(SimpleBackbone, self).__init__()
+
+        base = []
+
+        in_channel_list = layers[:-1]
+        out_channel_list = layers[1:]
+
+        for in_channels, out_channels in zip(in_channel_list, out_channel_list):
+
+            base.append(DownsampleBlock(in_channels, out_channels))
+
+        self.layers = nn.Sequential(*base)
+
+
+    def forward(self, x):
+
+        return self.layers(x)
