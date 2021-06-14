8114f127981c114a3b757e1b285318cc00d0839e
jakebiros <jakebiros@gmail.com>
2021-06-14 11:05:30 +0900

    Add tests for the backbones used in the SSD

---
+++ tests/test_backbones.py
@@ -0,0 +1,22 @@
+import unittest
+
+from models.backbone.simple import SimpleBackbone
+
+
+class TestBackbones(unittest.TestCase):
+
+    def setUp(self):
+
+        self.backbone = SimpleBackbone([3, 16, 32, 64])
+
+    def test_explicit_model_outputs_properly(self):
+
+        x = torch.ones(32, 3, 256, 256)
+
+        output = self.backbone(x)
+
+        print(output.shape)
+
+
+if __name__ == '__main__':
+    unittest.main()
