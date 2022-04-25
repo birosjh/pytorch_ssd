be2eecb49e6d04aa0d0e6965a6d7768e8c730912
Jacob Biros <jakebiros@gmail.com>
2022-04-25 22:33:51 +0900

    Add task to create test data

--- tasks.py
+++ tasks.py
@@ -8,14 +8,18 @@ def lint(c, filename="."):
 
 
 @task
-def test(c, filename="*"):
-
-    c.run(f"python -m unittest tests/test_{filename}.py")
-
-
-@task
 def format(c, filename="."):
 
     c.run(f"isort {filename}")
     c.run(f"black {filename}")
     c.run(f"flake8 {filename}")
+
+@task
+def test(c, filename="*"):
+
+    c.run(f"python -m unittest tests/test_{filename}.py")
+
+@task
+def create_test_data(c):
+
+    c.run("python3 scripts/prepare_test_data.py")
