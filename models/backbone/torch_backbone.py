4898f4c9e5e3a23177052fb5db7db5c352396dc7
Jacob Biros <jakebiros@gmail.com>
2022-05-01 16:25:41 +0900

    Update to use models from torchvision

--- models/backbone/torch_backbone.py
+++ models/backbone/torch_backbone.py
@@ -1,24 +1,19 @@
-import torch.nn as nn
+import torch.models as models
 
 from models.layers.downsample_block import DownsampleBlock
 
 
-class SimpleBackbone(nn.Module):
-    def __init__(self, layers):
+class TorchBackbone(nn.Module):
+    def __init__(self, config):
 
-        super(SimpleBackbone, self).__init__()
+        super(TorchBackbone, self).__init__()
 
-        base = []
+        load_model = getattr(models, config["backbone"])
 
-        in_channel_list = layers[:-1]
-        out_channel_list = layers[1:]
+        self.model = load_model(pretrained=config["pretrained"])
 
-        for in_channels, out_channels in zip(in_channel_list, out_channel_list):
-
-            base.append(DownsampleBlock(in_channels, out_channels))
-
-        self.layers = nn.Sequential(*base)
+        self.model = self.model.features
 
     def forward(self, x):
 
-        return self.layers(x)
+        return self.model(x)
