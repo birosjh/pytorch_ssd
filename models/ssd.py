6507edb00a239d32ad2712ede70033acd157609d
Jacob Biros <jakebiros@gmail.com>
2022-05-02 14:18:12 +0900

    Restructure ssd to separate feature extractor logic from ssd logic

--- models/ssd.py
+++ models/ssd.py
@@ -10,35 +10,28 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-from models.layers.downsample_block import DownsampleBlock
+from models.backbone.backbone_loader import backbone_loader
 
 
 class SSD(nn.Module):
-    def __init__(
-        self,
-        backbone: nn.Module,
-        aspect_ratio_setting_per_feature_map: list,
-        num_classes: int,
-    ):
+    def __init__(self, config: dict, num_classes: int) -> None:
         # Always have to do this when making a new model
         super(SSD, self).__init__()
+
+        self.feature_map_extractor = backbone_loader(config)
 
         self.loc_layers = []
         self.conf_layers = []
 
-        self.backbone = backbone
-
-        num_output_channels_per_layer = [64, 128, 128, 128, 128]
-
         num_defaults_per_feature_map = []
 
-        for aspect_ratio_setting in aspect_ratio_setting_per_feature_map:
-            num_defaults = 2 + len(aspect_ratio_setting) * 2
+        for aspect_ratio_setting in config["aspect_ratios"]:
 
+            num_defaults = 2 + len(aspect_ratio_setting) * 2
             num_defaults_per_feature_map.append(num_defaults)
 
         for num_anchors, output_channels in zip(
-            num_defaults_per_feature_map, num_output_channels_per_layer
+            num_defaults_per_feature_map, self.feature_map_extractor.output_channels()
         ):
 
             self.loc_layers += self.bbox_predictor(output_channels, num_anchors)
@@ -46,29 +39,18 @@ class SSD(nn.Module):
                 output_channels, num_anchors, num_classes
             )
 
-        self.block_1 = DownsampleBlock(64, 128)
-        self.block_2 = DownsampleBlock(128, 128)
-        self.block_3 = DownsampleBlock(128, 128)
-
     def forward(self, x):
         feature_maps = []
         loc = []
         conf = []
 
-        x = self.backbone(x)
-        feature_maps.append(x)
+        # Collect feature_maps
+        for section in self.feature_map_extractor.model:
 
-        x = self.block_1(x)
-        feature_maps.append(x)
+            x = section(x)
+            feature_maps.append(x)
 
-        x = self.block_2(x)
-        feature_maps.append(x)
-
-        x = self.block_3(x)
-        feature_maps.append(x)
-
-        x = F.max_pool2d(x, kernel_size=x.size()[2:])
-        feature_maps.append(x)
+        print(len(feature_maps))
 
         for idx, feature_map in enumerate(feature_maps):
             loc.append(
@@ -77,6 +59,9 @@ class SSD(nn.Module):
             conf.append(
                 self.conf_layers[idx](feature_map).permute(0, 2, 3, 1).contiguous()
             )
+
+        print(loc)
+        print(conf)
 
         loc = torch.cat([o.view(o.size(0), -1) for o in loc], 1)
         conf = torch.cat([o.view(o.size(0), -1) for o in conf], 1)
