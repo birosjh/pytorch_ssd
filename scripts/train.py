4ce3617200064e2e5138fe068dfd8c65f93e2f11
Jacob Biros <jakebiros@gmail.com>
2022-05-02 14:46:10 +0900

    Move data_encoder setup outside of the image dataset

--- scripts/train.py
+++ scripts/train.py
@@ -6,6 +6,7 @@ from explicit_ssd import SSD
 from trainer import Trainer
 
 from datasets.image_dataset import ImageDataset
+from utils.data_encoder import DataEncoder
 
 
 def load_configurations():
@@ -35,8 +36,11 @@ def main():
 
     print(config)
 
+    data_encoder = DataEncoder(model_config)
+
     dataset = ImageDataset(
         data_config=data_config,
+        data_encoder=data_encoder,
         transform=True,
     )
 
