a8c52089af689cb2d9178f1133f3f14b43e950f9
Jacob Biros <jakebiros@gmail.com>
2022-05-29 20:38:43 +0900

    Add loss test file

---
+++ tests/test_loss.py
@@ -0,0 +1,44 @@
+import unittest
+
+import yaml
+from torch.utils.data import DataLoader
+
+from datasets.image_dataset import ImageDataset
+from models.ssd import SSD
+from utils.data_encoder import DataEncoder
+from models.loss.confidence import localization_loss
+
+
+class TestLoss(unittest.TestCase):
+    def setUp(self):
+
+        with open("tests/test_config.yaml") as f:
+            config = yaml.safe_load(f)
+
+        model_config = config["model_configuration"]
+        training_config = config["training_configuration"]
+        data_config = config["data_configuration"]
+
+        data_encoder = DataEncoder(model_config)
+
+        dataset = ImageDataset(
+            data_config=data_config,
+            data_encoder=data_encoder,
+            transform=True,
+            mode="train",
+        )
+
+    def test_localization_loss(self):
+
+        predictions = 
+        targets = 
+
+
+        loss = localization_loss(predictions, targets)
+
+        self.assertEquals(loss, 1.20)
+
+
+
+if __name__ == "__main__":
+    unittest.main()
