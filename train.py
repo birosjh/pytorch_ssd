c1f828a9dd816280dea93d669cfd4c07a5bb4905
Jacob Biros <jakebiros@gmail.com>
2022-05-30 19:49:41 +0900

    Add validation to training loop and improve training logic

--- train.py
+++ train.py
@@ -36,19 +36,24 @@ def main():
 
     data_encoder = DataEncoder(model_config)
 
-    dataset = ImageDataset(
+    train_dataset = ImageDataset(
         data_config=data_config,
         data_encoder=data_encoder,
         transform=True,
+    )
+
+    val_dataset = ImageDataset(
+        data_config=data_config,
+        data_encoder=data_encoder,
+        transform=False,
+        mode="val"
     )
 
     num_classes = len(data_config["classes"]) + 1
 
     model = SSD(model_config, num_classes).to(device)
 
-    default_boxes = data_encoder.default_boxes
-
-    trainer = Trainer(model, dataset, default_boxes, training_config)
+    trainer = Trainer(model, train_dataset, val_dataset, training_config)
     trainer.train()
 
 
--- trainer/trainer.py
+++ trainer/trainer.py
@@ -14,12 +14,22 @@ from models.loss.ssd import SSDLoss
 
 
 class Trainer:
-    def __init__(self, model, dataset, default_boxes, training_config):
+    """
+    The trainer takes a model and datasets as an argument 
+    and trains the model according to the training configurations
+    """
+    def __init__(self, model, train_dataset, val_dataset, training_config):
 
         self.model = model
 
-        self.dataloader = DataLoader(
-            dataset,
+        self.train_dataloader = DataLoader(
+            train_dataset,
+            batch_size=training_config["batch_size"],
+            num_workers=training_config["num_workers"],
+        )
+
+        self.val_dataloader = DataLoader(
+            val_dataset,
             batch_size=training_config["batch_size"],
             num_workers=training_config["num_workers"],
         )
@@ -28,25 +38,82 @@ class Trainer:
             model.parameters(), lr=training_config["learning_rate"]
         )
 
-        self.default_boxes = default_boxes
-
         self.loss = SSDLoss(alpha=training_config["alpha"])
 
         self.epochs = training_config["epochs"]
 
     def train(self):
+        """
+        Train the model
+        """
 
         for epoch in range(self.epochs):
             print("Epoch {}/{}".format(epoch, self.epochs - 1))
             print("-" * 10)
 
-            for images, targets in tqdm(self.dataloader):
-                # Compute prediction and loss
-                predictions = self.model(images)
+            self.train_one_epoch()
 
-                conf_loss, loc_loss, loss = self.loss(predictions, targets)
+            self.validate_one_epoch()
 
-                # Backpropagation
-                self.optimizer.zero_grad()
-                loss.backward()
-                self.optimizer.step()
+                
+    def train_one_epoch(self):
+        """
+        Run the model through one epoch of training
+        """
+
+        epoch_conf_loss = 0
+        epoch_loc_loss = 0
+        epoch_loss = 0
+
+        for images, targets in tqdm(self.train_dataloader):
+            # Compute prediction and loss
+            predictions = self.model(images)
+
+            conf_loss, loc_loss, loss = self.loss(predictions, targets)
+
+            epoch_conf_loss += conf_loss
+            epoch_loc_loss += loc_loss
+            epoch_loss += loss
+
+            # Backpropagation
+            self.optimizer.zero_grad()
+            loss.backward()
+            self.optimizer.step()
+
+        epoch_conf_loss /= len(self.train_dataloader)
+        epoch_loc_loss /= len(self.train_dataloader)
+        epoch_loss /= len(self.train_dataloader)
+
+        print(f"Training Confidence Loss: {epoch_conf_loss}")
+        print(f"Training Localization Loss: {epoch_loc_loss}")
+        print(f"Training Total Loss: {epoch_loss}")
+
+
+    def validate_one_epoch(self):
+        """
+        Run the model through one epoch of validation
+        """
+
+        epoch_val_conf_loss = 0
+        epoch_val_loc_loss = 0
+        epoch_val_loss = 0
+
+        for images, targets in tqdm(self.val_dataloader):
+                
+            predictions = self.model(images)
+
+            conf_loss, loc_loss, loss = self.loss(predictions, targets)
+
+            epoch_val_conf_loss += conf_loss
+            epoch_val_loc_loss += loc_loss
+            epoch_val_loss += loss
+
+            # TODO: Calculate mAP
+
+        epoch_val_conf_loss /= len(self.val_dataloader)
+        epoch_val_loc_loss /= len(self.val_dataloader)
+        epoch_val_loss /= len(self.val_dataloader)
+        
+        print(f"Validation Confidence Loss: {epoch_val_conf_loss}")
+        print(f"Validation Localization Loss: {epoch_val_loc_loss}")
+        print(f"Validation Total Loss: {epoch_val_loss}")
