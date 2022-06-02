3cad393ec3b2fc8f3b5d1ee92286fe9098cf9795
Jacob Biros <jakebiros@gmail.com>
2022-06-02 17:09:15 +0900

    Add docstrings and type hints

--- datasets/transformations.py
+++ datasets/transformations.py
@@ -1,11 +1,15 @@
 import albumentations as A
+import numpy as np
 
 class Transformations():
+    """
+    Sets up an albumentations transformation composition when initialized.
+    It returns a transformed image when called
+    """
 
-    def __init__(self, config, mode):
+    def __init__(self, config: dict, mode: str):
             
         if config["transform"] and mode == "train":
-            print("hi")
             self.transform = A.Compose([
                 A.HorizontalFlip(p=0.5),
                 A.RandomBrightnessContrast(p=0.5),
@@ -20,6 +24,16 @@ class Transformations():
                 )
             ], bbox_params=A.BboxParams(format='pascal_voc'))
     
-    def __call__(self, image, bounding_boxes):
+    def __call__(self, image: np.ndarray, bounding_boxes: list) -> dict:
+        """
+        Apply the albumentations transformation and return the transformed image
+
+        Args:
+            image (np.ndarray): Image to be transformed
+            bounding_boxes (list): Bounding boxes for the image
+
+        Returns:
+            dict: Returns an albumentations dictionary with the image and bounding boxes inside
+        """
 
         return self.transform(image=image, bboxes=bounding_boxes)
--- visualize/visualize_batch.py
+++ visualize/visualize_batch.py
@@ -14,37 +14,71 @@ from utils.data_encoder import DataEncoder
 BOX_COLOR = (0, 0, 0)
 TEXT_COLOR = (0, 0, 0)
 
-def get_color_from_list(class_id, color_list):
+def get_color_from_list(class_id: int, color_list: list) -> tuple:
 
     return tuple((color_list[class_id] * 256)[0:-1].astype(int).tolist())
 
-def visualize_bbox(img, bbox, color_list, class_names, thickness=2):
-    """Visualizes a single bounding box on the image"""
+def visualize_bbox(image: np.ndarray, label: np.ndarray, color_list: list, class_names: list, thickness=2) -> np.ndarray:
+    """
+    Visualizes a single bounding box on the image
 
-    x_min, y_min, x_max, y_max, class_id = bbox
+    Args:
+        image (np.ndarray): Image to apply bounding boxes to
+        label (np.ndarray): A label containing bounding box coordinates and a class id
+        color_list (list): A list of colors for bounding boxes of each class
+        class_names (list): A list of class names
+        thickness (int, optional): Thickness of . Defaults to 2.
+
+    Returns:
+        np.ndarray: The image with the box drawn on it
+    """
+
+    x_min, y_min, x_max, y_max, class_id = label
     class_id = int(class_id)
     class_name = class_names[class_id]
 
     color = get_color_from_list(class_id, color_list)
 
-    cv2.rectangle(img, (x_min, y_min), (x_max, y_max), color=color, thickness=thickness)
+    cv2.rectangle(
+        image, 
+        (x_min, y_min), 
+        (x_max, y_max), 
+        color=color, 
+        thickness=thickness
+    )
     
-    ((text_width, text_height), _) = cv2.getTextSize(class_name, cv2.FONT_HERSHEY_SIMPLEX, 0.35, 1)    
-    cv2.rectangle(img, (x_min, y_min - int(1.3 * text_height)), (x_min + text_width, y_min), color, -1)
+    ((text_width, text_height), _) = cv2.getTextSize(class_name, cv2.FONT_HERSHEY_SIMPLEX, 0.35, 1) 
+
+    cv2.rectangle(
+        image, 
+        (x_min, y_min),
+        (x_min + text_width, y_min + int(1.3 * text_height)),
+        color=color, 
+        thickness=-1
+    )
+
     cv2.putText(
-        img,
+        image,
         text=class_name,
-        org=(x_min, y_min - int(0.3 * text_height)),
+        org=(x_min, y_min + int(text_height)),
         fontFace=cv2.FONT_HERSHEY_SIMPLEX,
         fontScale=0.35, 
         color=TEXT_COLOR, 
         lineType=cv2.LINE_AA,
     )
-    return img
+    return image
 
-def visualize_batch(config_file, use_val=False):
+def visualize_batch(config_path: str, val: bool = False) -> None:
+    """
+    Visualize a batch of images exactly as they are returned from the dataloader
+    This includes any transformations that may have been performed
 
-    with open(config_file) as file:
+    Args:
+        config_path (str): Path to the desired config  file
+        val (bool, optional): Whether to use a validation data loader or not. Defaults to False.
+    """
+
+    with open(config_path) as file:
         config = yaml.safe_load(file)
 
     model_config = config["model_configuration"]
@@ -53,7 +87,7 @@ def visualize_batch(config_file, use_val=False):
 
     mode = "train"
 
-    if use_val:
+    if val:
         mode = "val"
 
     data_encoder = DataEncoder(model_config)
