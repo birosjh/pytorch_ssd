61696e338db29b50b3d181194c839bcf3769d659
Jacob Biros <jakebiros@gmail.com>
2023-05-04 15:35:55 +0900

    Only apply nms to overlapping boxes

--- utils/nms.py
+++ utils/nms.py
@@ -12,6 +12,10 @@ def non_maximum_supression(confidences, localizations, iou_threshold, device):
 
         iou_grid = intersection_over_union(boxes, boxes)
 
+        is_overlapping = ~torch.isclose(
+            iou_grid.sum(dim=0), iou_grid.max(dim=0).values
+        )
+
         maximum_indices = []
 
         # Get higest confidence value for each box
@@ -19,28 +23,29 @@ def non_maximum_supression(confidences, localizations, iou_threshold, device):
 
         # Create a list of indices for each box
         confidence_indices = torch.arange(len(highest_confidence)).to(device)
+        overlapping_indices = confidence_indices[is_overlapping]
+        non_overlapping_indices = confidence_indices[~is_overlapping]
 
-        print("hi")
-
-        while len(confidence_indices) > 0 and len(maximum_indices) < len(confidence_indices):
+        while len(overlapping_indices) > 0:
 
             # Select highest confidence value in list
-            index_of_max = torch.max(highest_confidence[confidence_indices], 0).indices # Argmax in mps has a bug
-            maximum_indices.append(index_of_max)
+            index_of_max = torch.max(highest_confidence[overlapping_indices], 0).indices # Argmax in mps has a bug
+            maximum_indices.append(overlapping_indices[index_of_max])
 
             # Remove max from remaining indices
-            confidence_indices = confidence_indices[confidence_indices != index_of_max]
+            overlapping_indices = overlapping_indices[overlapping_indices != overlapping_indices[index_of_max]]
 
             # Get max iou values
-            iou = iou_grid[index_of_max][confidence_indices]
+            iou = iou_grid[index_of_max][overlapping_indices]
 
             # Remove overlapping
-            confidence_indices = confidence_indices[iou < iou_threshold]
+            overlapping_indices = overlapping_indices[iou < iou_threshold]
 
-        maximum_indices = list(set(maximum_indices))
+        maximum_indices = torch.tensor(maximum_indices).to(device)
+        maximum_indices = torch.cat([maximum_indices, non_overlapping_indices])
 
         min_localizations = torch.ones(localizations.shape[1], dtype=bool)
-        min_localizations[maximum_indices] = False
+        min_localizations[torch.tensor(maximum_indices)] = False
 
         localizations[idx][min_localizations] = 0
 
