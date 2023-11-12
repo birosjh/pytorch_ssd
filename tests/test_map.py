0c8c8f51658bb1907c99354965c3d695fa263550
Jacob Biros <jakebiros@gmail.com>
2023-11-12 17:41:30 +0900

    Adust map tests

--- tests/test_map.py
+++ tests/test_map.py
@@ -2,8 +2,9 @@ import unittest
 import yaml
 import torch
 
+from models.ssd import SSD
 from utils.data_encoder import DataEncoder
-from models.metrics.map import mean_average_precision
+from models.metrics.map import MeanAveragePrecision
 
 torch.set_printoptions(precision=10)
 
@@ -15,10 +16,40 @@ class TestMeanAveragePrecision(unittest.TestCase):
             config = yaml.safe_load(f)
 
         self.model_config = config["model_configuration"]
+        self.data_config = config["data_configuration"]
 
-    def test_mean_average_precision_output_is_correct(self):
+        self.map = MeanAveragePrecision(
+            self.data_config["classes"],
+            "cpu",
+            "coco"
+        )
+
+
+
+    def test_average_precision_output_is_correct(self):
+
+        confidences = torch.tensor(
+            [0.32, 0.84, 0.11, 0.51, 0.62, 0.78]
+        )
+        is_true_positive = torch.tensor([0, 1, 0, 1, 0, 1])
+        is_true_positive = is_true_positive > 0
+
+        ap = self.map.average_precision(confidences, is_true_positive)
+
+        print(ap)
+
+    def test_coco_map_output_is_correct(self):
+
+        fake_batch = torch.zeros(1, 3, 256, 256)
+        ground_truths = torch.zeros(1, 6132, 21)
+
+        num_classes = len(self.data_config["classes"]) + 1
 
         data_encoder = DataEncoder(self.model_config)
+        model = SSD(self.model_config, num_classes, data_encoder, "cpu")
 
-        predictions = 
+        confidences, localizations = model(fake_batch)
 
+        ap = self.map.coco_mean_average_precision(confidences, localizations, ground_truths)
+
+        # print(ap)
