986f776572d37b35c942c09b46eef7dcb634584e
Jacob Biros <jakebiros@gmail.com>
2024-08-04 17:44:33 +0900

    Refactor loss

--- src/models/loss/ssd.py
+++ src/models/loss/ssd.py
@@ -6,116 +6,81 @@ from torchvision.ops import generalized_box_iou_loss
 
 
 class SSDLoss(nn.Module):
-    def __init__(self, alpha, iou_threshold, data_encoder) -> None:
+    def __init__(self, alpha, iou_threshold, default_boxes, scale_xy, scale_wh) -> None:
         super(SSDLoss, self).__init__()
 
         self.confidence_loss = nn.CrossEntropyLoss(reduction="none")
-        self.localization_loss = generalized_box_iou_loss
+        self.localization_loss = nn.SmoothL1Loss(reduction='none')
 
         self.alpha = alpha
         self.iou_threshold = iou_threshold
-        self.data_encoder = data_encoder
+        self.default_boxes = default_boxes.unsqueeze(dim=0)
 
-    def forward_per_item(
-        self,
-        pred_confidences: torch.Tensor,
-        pred_localizations: torch.Tensor,
-        targets: torch.Tensor,
-    ) -> tuple:
-        target_confidences = targets[:, -1].type(torch.int64)
-        target_localizations = targets[:, 0:4]
+        self.scale_xy = scale_xy
+        self.scale_wh = scale_wh
 
-        object_exists = target_confidences > 0
+    def create_offsets(self, boxes):
+        """
+        Create Location Offsets
+        """
 
-        num_matched = object_exists.int().sum()
+        xy_offsets = self.scale_xy * (boxes[:, :, :2] - self.default_boxes[:, :, :2]) / self.default_boxes[:, :, 2:]
+        wh_offsets = self.scale_wh * (boxes[:, :, 2:]/self.default_boxes[:, :, 2:]).log()
 
-        loss = self.calculate_confidence_loss(
-            pred_confidences, target_confidences, object_exists, num_matched
-        )
+        return torch.cat((xy_offsets, wh_offsets), dim=2).contiguous()
 
-        conf_loss = loss.float().item()
 
-        localization_loss = self.calculate_localization_loss(
-            pred_localizations[object_exists],
-            target_localizations[object_exists],
-        )
+    def forward(self, confidences: torch.Tensor, localizations: torch.Tensor, targets: torch.Tensor):
+        """_summary_
 
-        loss += self.alpha * localization_loss
-        loc_loss = localization_loss.float().item()
+        Args:
+            confidences (torch.Tensor): Confidence values outputed from the model
+            localizations (torch.Tensor): Localization values outputed from the model
+            targets (torch.Tensor): _description_
 
-        return loss, conf_loss, loc_loss, num_matched
+        Returns:
+            loss: Total Loss
+            conf_loss: Confidence Loss
+            loc_loss: Localization Loss
+        """
 
-    def calculate_confidence_loss(
-        self, pred_confidences, target_confidences, positive_matches, num_matched
-    ):
-        positive_loss = self.calculate_positives_confidence_loss(
-            pred_confidences[positive_matches], target_confidences[positive_matches]
-        )
+        boxes = targets[:, :, :4]
+        labels = targets[:, :, 4].long()
+        
+        non_zero_label_indices = labels > 0
+        num_non_zero_targets = non_zero_label_indices.sum(dim=1)
+        
+        location_offsets = self.create_offsets(boxes)
 
-        negative_loss = self.calculate_negatives_confidence_loss(
-            pred_confidences[~positive_matches], num_matched
-        )
+        loc_loss = self.localization_loss(localizations, location_offsets).sum(dim=2)
 
-        loss = positive_loss + negative_loss
+        # Retain only loss of non-background targets
+        loc_loss = (non_zero_label_indices.float() * loc_loss).sum(dim=1)
 
-        return loss
+        conf_loss = self.confidence_loss(confidences.permute((0, 2, 1)), labels)
 
-    def calculate_positives_confidence_loss(
-        self, matched_confidences, matched_target_confidences
-    ):
-        # Calculate Positive Confidence Losses
-        positive_confidence_losses = self.confidence_loss(
-            matched_confidences, matched_target_confidences
-        )
+        negative_conf_loss = conf_loss.clone()
 
-        return positive_confidence_losses.sum()
+        # Don't select positive targets
+        negative_conf_loss[non_zero_label_indices] = 0
 
-    def calculate_negatives_confidence_loss(self, unmatched_confidences, num_matched):
-        # Calculate Negative Confidence Losses
-        unsorted_negative_confidence_losses = self.confidence_loss(
-            unmatched_confidences,
-            torch.zeros(unmatched_confidences.shape[0], dtype=int).to(
-                unmatched_confidences.device
-            ),
-        )
-        negative_confidence_losses = unsorted_negative_confidence_losses.sort(
-            descending=True
-        ).values
+        _, conf_idx = negative_conf_loss.sort(dim=1, descending=True)
+        _, conf_rank = conf_idx.sort(dim=1)
 
-        return negative_confidence_losses[0 : 3 * num_matched].sum()
+        # number of negative three times positive
+        neg_num = torch.clamp(
+            3 * num_non_zero_targets,
+            max=non_zero_label_indices.size(1)
+        ).unsqueeze(-1)
 
-    def calculate_localization_loss(
-        self, matched_localizations, matched_target_localizations
-    ):
-        localization_loss = self.localization_loss(
-            matched_localizations, matched_target_localizations, reduction="sum"
-        )
+        neg_target_indices = conf_rank < neg_num
 
-        return localization_loss
+        conf_loss = (conf_loss * ((non_zero_label_indices + neg_target_indices).float())).sum(dim=1)
 
-    def forward(self, confidences, localizations, targets):
-        batch_conf_loss = 0
-        batch_loc_loss = 0
-        batch_loss = 0
-        num_hits = 0
+        # avoid no object detected
+        total_loss = loc_loss + (self.alpha * conf_loss)
+        num_mask = (num_non_zero_targets > 0).float()
+        num_non_zero_targets = num_non_zero_targets.float().clamp(min=1e-6)
+        loss = (total_loss * num_mask / num_non_zero_targets).mean(dim=0)
 
-        for conf, loc, target in zip(confidences, localizations, targets):
-            loss, conf_loss, loc_loss, hit_count = self.forward_per_item(
-                conf, loc, target
-            )
-
-            batch_conf_loss += conf_loss
-            batch_loc_loss += loc_loss
-            batch_loss += loss
-            num_hits += hit_count
-
-        if num_hits == 0:
-            return torch.tensor(0), {"conf": 0, "loc": 0}
-
-        batch_conf_loss /= num_hits
-        batch_loc_loss /= num_hits
-        batch_loss /= num_hits
-
-        breakdown = {"conf": batch_conf_loss, "loc": batch_loc_loss}
-
-        return batch_loss, breakdown
+        return loss, loc_loss.detach().sum(), conf_loss.detach().sum()
